4963e3776003812c7c50e932b7e273f0ba2f2538
yura85 <47061499+yura85@users.noreply.github.com>
2019-02-06 20:13:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,20 @@
-console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+//console.log("Up and running");
+
+//console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+cardOne = cards[3];
+cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardsInPlay);
+var cardTwo;
+cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");}
+		else {
+			alert("Sorry try again");
+		}
+	}
