beb6c2e12cbc5f93c76bd3c856bc908426d4f196
yura85 <47061499+yura85@users.noreply.github.com>
2019-02-09 12:49:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMath = function() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -15,8 +36,11 @@ var flipCard = function (cardId)
 if (cardsInPlay.length === 2) {
 	checkForMath();
 	}
-	 console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	 console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	  console.log(cards[cardId].cardImage);
+	  console.log(cards[cardId].suit);
+	
 };
 
 flipCard(0);
